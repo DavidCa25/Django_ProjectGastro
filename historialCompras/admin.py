ee5ec652c0911f2139803216aa66730992da3373
Jesus David Casillas Rios <105253291+DavidCa25@users.noreply.github.com>
2023-11-17 15:06:38 -0600

    Delete historialCompras directory

--- historialCompras/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from historialCompras.models import HistorialCompras
-
-admin.site.register(HistorialCompras)
--- historialCompras/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from compras.models import Compra
-
-class HistorialCompras(models.Model):
-    IdCompra = models.ForeignKey(Compra, on_delete=models.CASCADE, default=1)
-    total = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return f"Compra #{self.IdCompra} - Total: {self.total}"
